ecd8f95c1d0619042f27538b2e70a73a0fcc7f1c
Orbit Boyzz <vkktask@gmail.com>
2026-06-25 04:00:16 -0400

    feat: add Vercel Analytics (@vercel/analytics/react)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot, hydrateRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 
@@ -10,6 +11,7 @@ const app = (
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 )
